a55301ccc5c3ec99d5af2672979a32d58c2c0dc0
dengmare <nightmare.deng@gmail.com>
2015-07-11 19:36:32 +0800

    Assignment 6

--- Assignment 6/Result.m
+++ Assignment 6/Result.m
@@ -1,4 +1,18 @@
-clc;
+clear;clc; 
 
-%% Get the movies IDs
-[num,txt,raw] = xlsread('Assignment 3.xls','movie-row');
+[num,txt,raw] = xlsread('Assignment 6.xlsx','Items');
+ItemFeature1 = num(4:end, 3);
+
+movieID = num(4:end, 1);
+
+%% Part I: Top movies for feature 1
+[sortedFeature, sortedFeatureIndex] = sort(ItemFeature1,'descend');
+
+movieID(sortedFeatureIndex(1:5));
+
+%% Part III: Top movies for user 4469
+weights = num(2,3:17);
+itemFeatures = num(4:end, 3:end);
+userFeatures = xlsread('Assignment 6.xlsx', 'Users', 'B2:P26');
+
+predictScores = itemFeatures .* userFeatures 
